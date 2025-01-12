4f4ecec1b8818021bba420eaf94b39019c812f07
mahsa azimi <mahsaazimi8574@gmail.com>
2025-01-12 06:41:19 -0800

    Fixed bug in login form validation

--- Programmer-Blog.Web/obj/Debug/net8.0/Programmer-Blog.Web.AssemblyInfo.cs
+++ Programmer-Blog.Web/obj/Debug/net8.0/Programmer-Blog.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Programmer-Blog.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9df9704da12ca01049533625c23f63f9b92aa6da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Programmer-Blog.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Programmer-Blog.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
